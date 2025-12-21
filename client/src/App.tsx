bb2b0ef7ce3609e910c16f61ac03618ba6f09639
Aditya Singh <singhaditya8382@gmail.com>
2025-12-21 12:10:46 +0530

    Initial commit: Setup MERN stack with TypeScript

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-3xl text-red-500 font-bold underline'>Hello</h1>
+      <div className='flex justify-center items-center h-screen bg-slate-500'>
+        <h1 className='text-3xl text-red-500 font-bold underline'>Aditya</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ server/src/server.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import connectDB from './config/db';
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('TS API is running...');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
